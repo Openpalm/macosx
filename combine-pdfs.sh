eb95a89c100fc51dc10e61f84090e81a919718fd
gog <gog@zion.local>
2019-09-03 14:27:36 +0200

    using argument list array

--- combine-pdfs.sh
+++ combine-pdfs.sh
@@ -2,17 +2,17 @@
 # combine two (more) pdfs.
 
 function usage() {
-  echo '[*] missing parameters'
-  echo '[*] usage: $0 <file1.pdf> <file2.pdf>'
+  echo '[*] you have to specify at least two files'
+  echo '[*] usage: $0 <file1.pdf> <file2.pdf> ... '
 }
 
 function combine() { 
-/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py -o out.pdf $1 $2
+/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py -o out.pdf $@
 }
 if [ -z $1 ] | [ -z $2 ]; then
   usage
 else 
-  combine $1 $2
+  combine $@
 fi 
 
 
